38cfdd712c6b217851e370f4439c3c134e944eea
Developer242424 <developer@fefdypartners.com>
2025-08-29 12:15:23 +0530

    new files are added from backup

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "fefdybraingym_root",
+//   password: "RwthZh4hGO2K",
+//   database: "fefdybraingym_techno",
+// });
+
 const db = mysql.createConnection({
   host: "localhost",
-  user: "fefdybraingym_root",
-  password: "RwthZh4hGO2K",
-  database: "fefdybraingym_techno",
+  user: "root",
+  password: "",
+  database: "fefdy_techno_demo",
 });
 
 db.connect((err) => {
--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -576,9 +576,7 @@ class DashboardController {
           orgCompletionPercentData,
           status: 200,
         });
-        //test from server
-        //test from server
-        //from local to github
+        //push from local to github
       } catch (err) {
         console.error("Error fetching single donut data:", err);
         return res
